573097bf98586001209be0fab3bc123a935ccc9b
Amy Spears <amspears007@gmail.com>
2022-10-19 20:17:30 -0600

    Add second exercise

--- friends.rb
+++ friends.rb
@@ -1,5 +1,8 @@
-arrays = ['amy', 'molly', 'penny', 'vivian','todd']
-puts arrays[2]
+# arrays = ['amy', 'molly', 'penny', 'vivian','todd']
+# puts arrays[2]
 
-puts arrays[4]
-puts arrays[0]
+# puts arrays[4]
+# puts arrays[0]
+
+places = ['vietnam', 'mexico', 'usa', 'canada', 'asia']
+puts places.push('lebanon')
